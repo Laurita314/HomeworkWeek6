f2f9d1e926f71938a16f30a4824ed65216174077
Laurita314 <laura.jim.hernandez@gmail.com>
2023-06-30 14:33:34 +0100

    Third commit

--- test_shop.py
+++ test_shop.py
@@ -1,24 +1,28 @@
 import unittest
 from shop import purchased, retry_purchase, ThreeFailedAttempts
 
-
 class TestingPurchased(unittest.TestCase):
-    # VALID CASES
-    def test_purchased_with_enough_money(self):
-        self.assertEqual(True, purchased(item="Phone", balance=100))
-
-    # INVALID/ERRONEOUS CASES
+    
+    # CASES THAT WERE PREVIPUSLY PROVIDES IN THE EXAMPLE BECKY ATTACHED
     def test_purchased_with_not_enough_money(self):
-        self.assertFalse(purchased(item="TV", balance=100))
+        self.assertFalse(purchased(item="Phone", balance=100))
     def test_purchased_with_negative_money(self):
-        self.assertEqual(False, purchased(item="Phone", balance=-100))
+        self.assertEqual(False, purchased(item="Watch", balance=-100))
 
-    # BOUNDARY CASE
-    def test_purchased_with_just_enough_money(self):
-        self.assertTrue(purchased(item="TV", balance=1000))
+    # NEW CASES I SUGGEST WE ADD:
+
+    # Case when user has 0 balance
+
+    def test_purchased_with_zero_balance(self):
+        self.assertFalse(purchased(item="Phone", balance=0))
+
+    #case when the person chooses an item that is not part of the available items:
+
+    def test_purchased_with_invalid_item(self):
+        self.assertFalse(purchased(item="Laptop", balance=100))
 
 class TestingRetryPurchase(unittest.TestCase):
-    # TESTING RAISED CUSTOM ERROR
+    # TESTING RAISED CUSTOM ERROR THAT BECKY PROVIDED
     def test_retrypurchased_too_many_attempts(self):
         with self.assertRaises(ThreeFailedAttempts):
             retry_purchase(item="TV", balance=100, attempts=3),
